339ecbaa5e9904d7666316f2b6c27a1550c8f49e
Jonas Silva <jonashsilva93@gmail.com>
2020-09-10 00:59:39 -0300

    adds graphiql-rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
 
   resources :graphql_client, only: :index
+
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
 end
